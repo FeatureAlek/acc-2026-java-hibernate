cbada8eb38ff68e2c32a475ba9650592d7036c51
Faustas <faustas.alekna@gmail.com>
2026-07-06 14:11:37 +0300

    data for testing

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO book (title, author, genre, publication_year, available) VALUES
+                                                                         ('Clean Code', 'Robert C. Martin', 'Programming', 2008, true),
+                                                                         ('Effective Java', 'Joshua Bloch', 'Programming', 2017, true),
+                                                                         ('The Hobbit', 'J.R.R. Tolkien', 'Fantasy', 1937, true),
+                                                                         ('1984', 'George Orwell', 'Dystopian', 1949, false),
+                                                                         ('Pride and Prejudice', 'Jane Austen', 'Romance', 1813, true);
